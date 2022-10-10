9ad631b475a1e2da46b459e24d4d8de0e94e0abf
mike@czyz.icu <mike@czyz.icu>
2022-10-10 11:12:32 +0200

    vscode setup & fibbonacci code setup

--- lab-01/main.cpp
+++ lab-01/main.cpp
@@ -3,6 +3,23 @@
 using namespace std;
 
 int main() {
-  cout << "Hello, world!" << endl;
+  cout << "Hello, world!" << endl << "Please provide how long the loop should work:";
+
+  int a = 0;
+  int b = 1;
+
+  int t;
+  cin >> t;
+
+  cout << a << endl << b << endl;
+
+  for (int i = 0; i < t; i++) {
+    b = a + b;
+    a = b;
+    // 0 1 1 2 3 
+
+    cout << a << endl;
+  }
+
   return 0;
 }
