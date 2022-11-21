93b9f23faaa93c453d0b1841788d90b8cf63dd0e
mike@czyz.icu <mike@czyz.icu>
2022-11-21 11:15:36 +0100

    lab-06: structs

--- lab-06/zadanie-05.cpp
+++ lab-06/zadanie-05.cpp
@@ -1,14 +1,52 @@
 #include <iostream>
+#include <random>
+#include <cmath>
+
+struct Point
+{
+  double x;
+  double y;
+} point;
+
+struct Square
+{
+  Point point1;
+  Point point2;
+} square;
+
+struct Circle
+{
+  Point point;
+  double length;
+} circle;
+
+struct Time
+{
+  int hours;
+  int minutes;
+  int seconds;
+};
+
+double distance_between_two_points(Point a, Point b)
+{
+  return sqrt(pow((a.x - b.x), 2) + pow((a.y - b.y), 2));
+}
 
 int main()
 {
-  struct {
-    int x;
-    int y;
-  } punkt;
+  std::default_random_engine generator;
+  std::uniform_real_distribution<double> distribution(0.0, 100.00);
+  Point a, b;
+  for (int i = 0; i < 10; ++i)
+  {
+    a.x = distribution(generator);
+    a.y = distribution(generator);
 
-  punkt.x = 5;
-  punkt.y = 10;
+    b.x = distribution(generator);
+    b.y = distribution(generator);
+
+    std::cout << "(" << a.x << ", " << a.y << ") and (" << b.x << ", " << b.y << ") = " << distance_between_two_points(a, b) << std::endl;
+  }
 
   return 0;
 }
