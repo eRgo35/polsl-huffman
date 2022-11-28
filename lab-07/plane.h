f6b745d8ec0adf5b85566351e5628a73ca18f5e8
mike@czyz.icu <mike@czyz.icu>
2022-11-28 11:01:49 +0100

    lab-07: Doxygen documentation

--- lab-07/plane.h
+++ lab-07/plane.h
@@ -5,6 +5,10 @@
 #include <vector>
 #include <cmath>
 
+/**
+ * @brief zbiór rodzajów położenia dwóch kół
+ * 
+ */
 enum circle_position
 {
   coaxial,
@@ -15,40 +19,115 @@ enum circle_position
   intersecting
 };
 
+/**
+ * @brief struktura opisująca punkt
+ * 
+ */
 struct Point
 {
   double x, y;
 };
 
+/**
+ * @brief struktura opisująca kwadrat
+ * 
+ */
 struct Square
 {
   Point point1, point2;
 };
 
+/**
+ * @brief struktura opisująca koło
+ * 
+ */
 struct Circle
 {
   Point point;
   double length;
 };
 
+/**
+ * @brief zwraca losową liczbę w przedziale podanym w zmiennej distribution
+ * 
+ * @return double 
+ */
 double generate_random_number();
 
+/**
+ * @brief generuje wektor N - punktów
+ * 
+ * @param N 
+ * @return std::vector<Point> 
+ */
 std::vector<Point> generate_points_vector(int N);
 
+/**
+ * @brief generuje wektor M - kwadratów
+ * 
+ * @param M 
+ * @return std::vector<Square> 
+ */
 std::vector<Square> generate_squares_vector(int M);
 
+/**
+ * @brief generuje wektor O - kół
+ * 
+ * @param O 
+ * @return std::vector<Circle> 
+ */
 std::vector<Circle> generate_circles_vector(int O);
 
+/**
+ * @brief zwraca odległość pomiędzy dwoma punktami
+ * 
+ * @param a 
+ * @param b 
+ * @return double 
+ */
 double distance_between_two_points(Point &a, Point &b);
 
+/**
+ * @brief zwraca punkt środkowy pomiędzy dwoma punktami
+ * 
+ * @param a 
+ * @param b 
+ * @return Point 
+ */
 Point center_between_two_points(Point &a, Point &b);
 
+/**
+ * @brief zwraca pole kwadratu
+ * 
+ * @param square 
+ * @return double 
+ */
 double area_of_a_square(Square &square);
 
+/**
+ * @brief zwraca większy kwadrat
+ * 
+ * @param square1 
+ * @param square2 
+ * @return Square 
+ */
 Square bigger_square(Square &square1, Square &square2);
 
+/**
+ * @brief zwraca pole koła
+ * 
+ * @param circle 
+ * @return double 
+ */
 double area_of_a_circle(Circle &circle);
 
+/**
+ * @brief zwraca wzajemne położenie między dwoma okręgami
+ * 
+ * @param circle1 
+ * @param circle2 
+ * @return circle_position 
+ */
 circle_position mutual_position_of_two_circles(Circle &circle1, Circle &circle2);
 
 #endif
--- lab-07/time.h
+++ lab-07/time.h
@@ -1,13 +1,29 @@
 #ifndef TIME_H
 #define TIME_H
 
+/**
+ * @brief struktura opisująca czas w schemacie godzina, minuta, sekunda
+ * 
+ */
 struct Time
 {
   int hours, minutes, seconds;
 };
 
+/**
+ * @brief dodaje drugi czas do pierwszego
+ * 
+ * @param time1 
+ * @param time2 
+ */
 void add_time(Time &time1, Time &time2);
 
+/**
+ * @brief odejmuje drugi czas od pierwszego
+ * 
+ * @param time1 
+ * @param time2 
+ */
 void sub_time(Time &time1, Time &time2);
 
 #endif
